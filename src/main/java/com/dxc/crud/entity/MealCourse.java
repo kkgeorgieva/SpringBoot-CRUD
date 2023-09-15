0e51283103af285acf9299f09caebbc05079c391
Kristina Georgieva <kristina.georgieva2@dxc.com>
2023-09-15 11:10:17 +0300

    Meal Course adding elements to database bug fixed

--- src/main/java/com/dxc/crud/entity/MealCourse.java
+++ src/main/java/com/dxc/crud/entity/MealCourse.java
@@ -1,24 +1,36 @@
 package com.dxc.crud.entity;
-
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "meal_course")
 public class MealCourse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
     private int id;
+    @Column(name = "course_name")
     private String mealName;
 
    /*  @OneToMany(mappedBy = "mealCourse")
     private Set<Recipe> recipes;
     */
 
+    //Default constructor 
+    public MealCourse(){
+
+    }
+    //Constructor with name
+    public MealCourse(String mealName){
+        this.mealName = mealName;
+    }
     public int getId(){
         return id;
     }
